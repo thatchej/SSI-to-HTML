33a94da1d2031e8177b6e4b1c6ed6ec66a101324
Jaron Thatcher <thatchej@onid.oregonstate.edu>
2014-07-08 10:07:53 -0700

    Update ssi-to-html.py

--- ssi-to-html.py
+++ ssi-to-html.py
@@ -1,7 +1,7 @@
 #This script will traverse through a directory heirarchy starting at the current directory
 #and change all Server Side Includes in .html files to the actual HTML they represent
 #
-#Sort of functions as an alternative to the server side include mechanism, but does it
+#Functions as an alternative to the server side include mechanism, but does it
 #statically instead of dynamically
 #
 #Author: Jaron Thatcer
